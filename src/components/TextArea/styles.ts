9a6f19315b2dfd8d12c9f79d22aa3205ace6931c
YaroslavHos <yaroslav.hos@stryber.com>
2022-05-11 12:30:57 +0200

    Added textarea component (stories, tests)

---
+++ src/components/TextArea/styles.ts
@@ -0,0 +1,78 @@
+import { createStyles } from '../../styles';
+import { ITextArea } from './index';
+
+export default createStyles((theme) => ({
+  container: (props: ITextArea) => ({
+    boxSizing: 'border-box',
+    width: 320,
+    height: 143,
+    borderRadius: 4,
+    position: 'relative',
+    fontFamily: theme.font,
+    '&:focus-within:not($containerError)': {
+      border: `1px solid ${theme[props.color!].main}`,
+      outline: `4px solid ${theme[props.color!].light}`,
+    },
+    '&:not($containerError)': {
+      border: `1px solid ${theme.default.main}`,
+      padding: '5px 8px',
+    }
+  }),
+  containerDisabled: {
+    pointerEvents: 'none',
+    userSelect: 'none',
+    backgroundColor: theme.default.light,
+  },
+  containerError: {
+    border: `1px solid ${theme.error.main}`,
+    padding: '5px 8px 5px 16px',
+    '&:focus-within': {
+      outline: `4px solid ${theme.error.light}`,
+    },
+    '&:before': {
+      content: '""',
+      position: 'absolute',
+      height: 143,
+      width: 8,
+      left: -1,
+      top: -1,
+      backgroundColor: theme.error.main,
+      borderTopLeftRadius: 4,
+      borderBottomLeftRadius: 4,
+    }
+  },
+  textarea: {
+    fontFamily: theme.font,
+    border: 'none',
+    outline: 'none',
+    textOverflow: 'ellipsis',
+    height: 110,
+    padding: '25px 0 0 0',
+    width: '100%',
+    color: theme.text.primary,
+    fontSize: 14,
+    resize: 'none',
+  },
+  label: {
+    color: theme.text.secondary,
+    position: 'absolute',
+    height: 17,
+    fontSize: 14,
+  },
+  textDisabled: {
+    color: theme.text.disabled,
+  },
+  message: {
+    marginTop: 8,
+    height: 17,
+    fontSize: 14,
+    fontFamily: theme.font,
+  },
+  maxLength: {
+    marginTop: 8,
+    color: theme.text.hint,
+    height: 17,
+    fontSize: 14,
+    fontFamily: theme.font,
+  }
+}));
---
+++ src/stories/TextArea.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { TextArea } from '../components/TextArea';
+
+export default {
+  title: 'Components/TextArea',
+  component: TextArea,
+} as ComponentMeta<typeof TextArea>;
+
+const Template: ComponentStory<typeof TextArea> = (args) => <TextArea {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Your message',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: 'Disabled',
+  disabled: true,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  label: 'With error',
+  errorMessage: 'This is an error message',
+};
+
+export const MaxLength = Template.bind({});
+MaxLength.args = {
+  label: 'Your message',
+  maxLength: 200,
+};
+
+export const WithValue = Template.bind({});
+WithValue.args = {
+  label: 'With value',
+  value: '"And now I will show you what is really a very interesting study, Mr. Windibank," Holmes continued. "I think of writing another little monograph some of these days on the typewriter and its relation to crime.',
+};
