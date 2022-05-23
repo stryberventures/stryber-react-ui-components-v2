7bb3c2fc154bc19c547d3044470b433f6d816f5e
sergeysychevskiy <sergey.sychevskiy@stryber.com>
2022-05-23 11:34:18 +0300

    Added tests for Toggle

---
+++ src/components/Toggle/Toggle.test.tsx
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom'
+import * as React from 'react'
+import { fireEvent, render } from '@testing-library/react'
+import { Toggle } from './index'
+
+it('should be rendered', () => {
+  const { getByRole } = render(<Toggle />);
+  expect(getByRole('checkbox',{ hidden: true })).toBeInTheDocument();
+});
+
+it('should not be checked by default', () => {
+  const { getByRole } = render(<Toggle />);
+  const checkBox = getByRole('checkbox',{ hidden: true }) as HTMLInputElement;
+  expect(checkBox.checked).toBe(false);
+});
+
+it('should be checked', () => {
+  const { getByRole } = render(<Toggle checked={true} />);
+  const checkBox = getByRole('checkbox',{ hidden: true }) as HTMLInputElement;
+  expect(checkBox.checked).toBe(true);
+});
+
+it('should change checked state', () => {
+  const { getByRole } = render(<Toggle />);
+  const checkBox = getByRole('checkbox',{ hidden: true }) as HTMLInputElement;
+  fireEvent.change(checkBox, { target: { checked: true } });
+  expect(checkBox.checked).toBe(true);
+});
