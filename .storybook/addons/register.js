00944f366bb15816863bd211b3a5bb27d54f5398
sergeysychevskiy <sergey.sychevskiy@stryber.com>
2022-07-11 12:39:14 +0300

    Added Storybook addon with npm install tab

---
+++ .storybook/addons/register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { addons, types } from '@storybook/addons';
+import NpmInstall from "../../src/stories/addons/NpmInstall";
+
+addons.register('npm', () => {
+    addons.add('npm', {
+        type: types.TAB,
+        title: 'Npm install',
+        route: ({ storyId, refId }) => (refId ? `/npm/${refId}_${storyId}` : `/npm/${storyId}`),
+        match: ({ viewMode }) => viewMode === 'npm',
+        render: () => <NpmInstall />,
+    });
+});
---
+++ src/stories/addons/NpmInstall/styles.ts
@@ -0,0 +1,11 @@
+import { createUseStyles } from 'react-jss';
+
+export default createUseStyles({
+  container: {
+    padding: '1rem',
+  },
+  title: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
