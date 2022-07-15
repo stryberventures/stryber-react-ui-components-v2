a3b1f7419e1367eac8bf3bae15c68cdbf461b729
sergeysychevskiy <sergey.sychevskiy@stryber.com>
2022-07-15 11:45:44 +0300

    Form - added onSubmit onReset tests

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,30 +1,57 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Form } from './index';
 import { Input } from '../Input';
+import { Button } from '../Button';
+import { IFormProps } from './types';
 
-const getForm = () => (
-  <Form
-    initialValues={{
-      email: 'some@mail.com',
-      password: 'somePass',
-    }}
-  >
-    <Input label={'Email'} name="email"/>
-    <Input label={'Password'} name="password"/>
-  </Form>
-)
+interface IProps extends Omit<IFormProps, 'children'> {
+  type?: 'submit' | 'reset';
+}
+
+const FormTemplate = (props: IProps) => {
+  const { type, ...rest } = props;
+
+  return (
+    <Form
+      {...rest}
+    >
+      <Input label={'Email'} name="email"/>
+      <Input label={'Password'} name="password"/>
+      {type === 'submit' && <Button type="submit" label="Submit"/>}
+      {type === 'reset' && <Button type="reset" label="Reset"/>}
+    </Form>
+  )
+};
 
 it('should render children', () => {
-  render(getForm());
+  render(<FormTemplate/>);
   expect(screen.queryByText('Email')).toBeInTheDocument();
   expect(screen.queryByText('Password')).toBeInTheDocument();
 });
 
 it('should render values in inputs', () => {
-  render(getForm());
+  const initialValues = {
+    email: 'some@mail.com',
+    password: 'somePass',
+  };
+  render(<FormTemplate initialValues={initialValues}/>);
   const [email, password] = screen.queryAllByRole('textbox') as [HTMLInputElement, HTMLInputElement];
-  expect(email.value).toBe('some@mail.com');
-  expect(password.value).toBe('somePass');
+  expect(email.value).toBe(initialValues.email);
+  expect(password.value).toBe(initialValues.password);
+});
+
+it('should fire onSubmit', () => {
+  const onSubmit = jest.fn();
+  render(<FormTemplate onSubmit={onSubmit} type="submit"/>);
+  fireEvent.click(screen.getByRole('button'));
+  expect(onSubmit).toHaveBeenCalled();
+});
+
+it('should fire onReset', () => {
+  const onReset = jest.fn();
+  render(<FormTemplate onReset={onReset} type="reset"/>);
+  fireEvent.click(screen.getByRole('button'));
+  expect(onReset).toHaveBeenCalled();
 });
