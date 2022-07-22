bb9b760d992e1e4f4d29b3e1be60a1889292e0c9
YaroslavHos <yaroslav.hos@stryber.com>
2022-07-22 14:52:02 +0200

    component refactoring

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -14,9 +14,13 @@ export interface ISlider extends IInput {
   thumbLabels?: 'inputs' | 'labels' | 'none',
   onChange?: (e: React.BaseSyntheticEvent) => void,
 }
-export interface IStyles {
-  minLeft: string,
+export interface ISliderExtended {
+  /*minLeft: string,
   maxLeft: string,
+  styleLeftThumb: string,
+  styleRightThumb: string,*/
+  sliderThumbMin: string,
+  sliderThumbMax: string,
 }
 export const Slider = (props: ISlider) => {
   const {
@@ -27,13 +31,16 @@ export const Slider = (props: ISlider) => {
     showSideLabels,
     showStepMarks,
     thumbLabels,
+    color,
     name = 'slider',
   } = props;
   const { fieldValue, unsetFormValue, updateFormValue } = useFormContext(name);
   const [minVal, setMinVal] = useState(Array.isArray(fieldValue) ? fieldValue[1] : fieldValue || min);
   const [maxVal, setMaxVal] = useState(Array.isArray(fieldValue) ? fieldValue[1] : fieldValue || max);
-  const [minSize, setMinSize] = useState(minVal);
-  const [maxSize, setMaxSize] = useState(maxVal);
+  const [minSize, setMinSize] = useState(minVal.toString().length * 13);
+  const [maxSize, setMaxSize] = useState(maxVal.toString().length * 13);
+  const inputMinSize = minVal.toString().length * 13
+  const inputMaxSize = maxVal.toString().length * 13
   const [showMin, setShowMin] = useState(false);
   const [showMax, setShowMax] = useState(false);
   const [error, setError] = useState(false);
@@ -44,13 +51,18 @@ export const Slider = (props: ISlider) => {
       return ((value - min) * 100 / (max - min));
     }, [min, max]);
 
-  const labelPosition = (value: number) => `calc(${getPercent(value)}% + (${15 - getPercent(value) * 0.3}px))`;
+  /*const labelPosition = (value: number) => `calc(${getPercent(value)}% + (${15 - getPercent(value) * 0.3}px))`;
   const [minLeft, setMinLeft] = useState(min.toString());
   const [maxLeft, setMaxLeft] = useState(max.toString());
-  const classes = useStyles( { minLeft, maxLeft });
+  const [styleLeftThumb, setStyleLeftThumb] = useState('0');
+  const [styleRightThumb, setStyleRightThumb] = useState('0');*/
+  const [positionThumbMin, setPositionThumbMin] = useState('0');
+  const [positionThumbMax, setPositionThumbMax] = useState('calc(100% - 30px)');
+
+  const classes = useStyles();
 
 
-  if (sliderVersion === 'range') {
+  /*if (sliderVersion === 'range') {
     // Set width of the range to decrease from the left side
     useEffect(() => {
       if (range.current) {
@@ -73,7 +85,20 @@ export const Slider = (props: ISlider) => {
         range.current.style.width = `${getPercent(minVal)}%`;
       }
     }, [minVal, getPercent]);
-  }
+  }*/
+  useEffect(() => {
+    if (sliderVersion === 'range') {
+      if (range.current) {
+        range.current.style.left = `${getPercent(+minVal)}%`;
+        range.current.style.width = `${getPercent(+maxVal) - getPercent(+minVal)}%`;
+      }
+    } else if (sliderVersion === 'basic') {
+      if (range.current) {
+        range.current.style.left = '0';
+        range.current.style.width = `${getPercent(minVal)}%`;
+      }
+    }
+  }, [minVal, maxVal, getPercent]);
   const dots = [];
   if (step > 1 && showStepMarks) {
     for(let i = min; i < max / step + 1; i++) {
@@ -89,14 +114,14 @@ export const Slider = (props: ISlider) => {
 
   const onMinChange = (e: React.BaseSyntheticEvent) => {
     const value = e.target.value;
-    setMinSize(value);
+    setMinSize(value.toString().length * 13);
     if(isNaN(value) || value > maxVal || value < min) {
       setError(true);
     } else setError(false);
   };
   const onMaxChange = (e: React.BaseSyntheticEvent) => {
     const value = e.target.value;
-    setMaxSize(value);
+    setMaxSize(value.toString().length * 13);
     if(isNaN(value) || value < minVal || value > max) {
       setError(true);
     }  else setError(false);
@@ -113,7 +138,7 @@ export const Slider = (props: ISlider) => {
       res = value
     }
     setMinVal(res);
-    setMinLeft(labelPosition(res));
+    //setMinLeft(labelPosition(res));
     setError(false);
     updateFormValue(name, res);
   };
@@ -128,33 +153,105 @@ export const Slider = (props: ISlider) => {
       res = value
     }
     setMaxVal(res);
-    setMaxLeft(labelPosition(res));
+    //setMaxLeft(labelPosition(res));
     setError(false);
     updateFormValue(name, res);
   };
-
-  const thumbMin = (e: React.BaseSyntheticEvent) => {
-    const value = Math.min(+e.target.value, +maxVal - step);
+  /*const thumbMin = (e: React.BaseSyntheticEvent) => {
+    const value = Math.min(+e.target.value, +maxVal - (sliderVersion === 'basic' ? 0 : step));
+    if (sliderVersion === 'range' && range.current && range.current.offsetParent && range.current.offsetParent.clientWidth < 30 * ((max - min) / step) && value > min) {
+      //setStyleLeftThumb(`calc(${0.3 * getPercent(value)}px - ${getPercent(step)}%)`);
+      //console.log(`calc(${0.3 * getPercent(value)}px - ${getPercent(step)}%)`, 'min');
+      //let shift = 30 / ((max - min) / step);
+      const newShift = 30 / (100 / getPercent(value))
+      const stepShift = step / 100 / getPercent(value)
+      console.log(`calc(${newShift}px - ${getPercent(step)}%)`, 'minShift');
+      if (minVal === min || maxVal === max) {
+        setStyleLeftThumb(`calc(${getPercent(step)}% - ${newShift}px)`);
+      } else setStyleLeftThumb(`calc(${newShift}px - ${stepShift}%)`);
+    } else setStyleLeftThumb('0');
     setMinVal(value);
     setMinSize(value);
     setMinLeft(labelPosition(value));
     setShowMin(true);
-  }
-  const thumbMax = (e: React.BaseSyntheticEvent) => {
-    const value = Math.max(+e.target.value, +minVal + step);
+    setSliderThumbMin(getPercent(value))
+  }*/
+  /*const thumbMax = (e: React.BaseSyntheticEvent) => {
+    const value = Math.max(+e.target.value, +minVal + (sliderVersion === 'basic' ? 0 : step));
+    if (sliderVersion === 'range' && range.current && range.current.offsetParent && range.current.offsetParent.clientWidth < 30 * ((max - min) / step) && value < max) {
+      //setStyleRightThumb(`calc(${0.3 * getPercent(max - value)}px - ${getPercent(step)}%)`);
+      //console.log(`calc(${0.3 * getPercent(max - value)}px - ${getPercent(step)}%)`, 'max');
+      const newShift = 30 / (100 / getPercent(max - value));
+      const stepShift = step / 100 / getPercent(max - value)
+      console.log(`calc(${newShift}px - ${getPercent(step)}%)`, 'maxShift')
+      if (minVal == min || maxVal == max) {
+        setStyleRightThumb(`calc(${getPercent(step)}% - ${newShift}px)`);
+      } else setStyleRightThumb(`calc(${newShift}px - ${stepShift}%)`);
+    } else setStyleRightThumb('0');
     setMaxVal(value);
     setMaxSize(value);
     setMaxLeft(labelPosition(value));
     setShowMax(true);
-  }
+    setSliderThumbMax(getPercent(max - value))
+  }*/
 
   const keyPress = (e: React.BaseSyntheticEvent) => {
     e.currentTarget.name === 'maxSliderInput' ? onMaxSubmit(e) : onMinSubmit(e)
   }
+  const handleDrag = (e: any) => {
+    e.currentTarget.onpointermove = e.currentTarget.id === 'minThumb' ? moveMin : moveMax
+    e.currentTarget.setPointerCapture(e.pointerId);
+  }
+  const moveMin = (value: any) => {
+    const [res, position] = checking(value)
+    //const newVal = checking(value)
+    setPositionThumbMin(`${position}px`)
+    setMinVal(res);
+    setMinSize(res.toString().length * 13);
+    setShowMin(true);
+  }
+  const moveMax = (value: any) => {
+    const [res, position] = checking(value)
+    //const newVal = checking(value);
+    setPositionThumbMax(`${position}px`)
+    setMaxVal(res);
+    setMaxSize(res.toString().length * 13);
+    setShowMax(true);
+  }
+  const checking = (e: any) => {
+    const parent = e.currentTarget.parentElement.getBoundingClientRect();
+    const parentWidth = parent.width;
+    const left = +e.clientX - parent.left - 15;
+    const correction = 100 / parentWidth * 30;
+    const value = Math.round((left / ((parentWidth - 30) / ((max - min) / step))) * step);
+    console.log(value, 'value');
+    let position = null
+    if (left > parentWidth - 30) {
+      position = parentWidth - 30
+    } else if (left < 0) {
+      position = 0
+    } else position = left
 
+
+    let res = value;
+    if (step > 1) {
+      const stepCount = Math.round(value / step);
+      res = Math.round(stepCount * step);
+    }
+    if (res < min) {
+      res = min
+    } else if (res > max) {
+      res = max
+    }
+    return [res, position]
+  }
+  const handleStopDrag = (e: any) => {
+    e.currentTarget.onpointermove = null
+    e.currentTarget.releasePointerCapture(e.pointerId);
+  }
   return (
     <div className={classes.container}>
-      <Input
+      {/*<Input
         type="range"
         name='min'
         label=''
@@ -163,9 +260,9 @@ export const Slider = (props: ISlider) => {
         value={minVal.toString()}
         step={step}
         controlled={true}
-        className={classes.thumb}
+        className={classnames(classes.thumb, classes.minThumb)}
         onClick={() => setShowMin(true)}
-        onChange={thumbMin}
+        //onChange={thumbMin}
         onMouseEnter={() => {
           setShowMin(true);
           setShowMax(false);
@@ -183,17 +280,83 @@ export const Slider = (props: ISlider) => {
           value={maxVal.toString()}
           step={step}
           controlled={true}
-          className={classes.thumb}
+          className={classnames(classes.thumb, classes.maxThumb)}
           onClick={() => setShowMax(true)}
-          onChange={thumbMax}
+          //onChange={thumbMax}
           onMouseEnter={() => {
             setShowMax(true);
             setShowMin(false);
             setMaxLeft(labelPosition(+maxLeft))
           }}
         />
-      )}
+      )}*/}
       <div className={classes.slider}>
+        <div
+          id="minThumb"
+          className={classnames(classes.thumb, classes.sliderThumbMin)}
+          onPointerDown={handleDrag}
+          onPointerUp={handleStopDrag}
+          onMouseEnter={() => {
+            setShowMin(true);
+            setShowMax(false);
+          }}
+          style={{ left: positionThumbMin }}
+        >
+          {thumbLabels === 'labels' && (
+            <div data-testid="testThumbLabelID" className={classnames(classes.thumbMinLabel, { [classes.show]: showMin })}>{minVal}</div>
+          )}
+          {thumbLabels === 'inputs' && (
+            <div data-testid="testThumbInputID"
+              className={classnames(classes.inputMinValue, { [classes.show]: showMin, [classes.error]: error })}>
+              <Input
+                key={minVal}
+                label=''
+                name='minSliderInput'
+                //size={minSize.toString().length || 1}
+                value={minVal.toString()}
+                onChange={onMinChange}
+                onBlur={onMinSubmit}
+                onClick={(e) => e.preventDefault()}
+                onKeyDown={(e) => {e.key === 'Enter' && keyPress(e)}}
+                style={{ width: minSize }}
+              />
+            </div>
+          )}
+        </div>
+        {sliderVersion === 'range' && (
+          <div
+            data-testid="testSecondInputID"
+            id="maxThumb"
+            className={classnames(classes.thumb, classes.sliderThumbMax)}
+            onPointerDown={handleDrag}
+            onPointerUp={handleStopDrag}
+            onMouseEnter={() => {
+              setShowMax(true);
+              setShowMin(false);
+            }}
+            style={{ left: positionThumbMax }}
+          >
+            {thumbLabels === 'labels' && (
+              <div className={classnames(classes.thumbMaxLabel, { [classes.show]: showMax })}>{maxVal}</div>
+            )}
+            {thumbLabels === 'inputs' && (
+              <div className={classnames(classes.inputMaxValue, { [classes.show]: showMax, [classes.error]: error })}>
+                <Input
+                  key={maxVal}
+                  label=''
+                  name='maxSliderInput'
+                  //size={maxSize.toString().length || 1}
+                  value={maxVal.toString() || '0'}
+                  onChange={onMaxChange}
+                  onBlur={onMaxSubmit}
+                  onClick={() => setShowMax(true)}
+                  onKeyDown={(e) => {e.key === 'Enter' && keyPress(e)}}
+                  style={{ width: maxSize }}
+                />
+              </div>
+            )}
+          </div>
+        )}
         <div className={classes.sliderTrack} />
         <div ref={range} className={classes.sliderRange} />
         {step > 1 && showStepMarks && (<div data-testid="testStepMarksID" className={classes.dotsContainer}>{dots}</div>)}
@@ -203,15 +366,15 @@ export const Slider = (props: ISlider) => {
             <div className={classes.sliderRightValue}>{max}</div>
           </>
         )}
-        {thumbLabels === 'labels' && (
+        {/*{thumbLabels === 'labels' && (
           <>
             <div data-testid="testThumbLabelID" className={classnames(classes.thumbMinLabel, { [classes.show]: showMin })}>{minVal}</div>
             {sliderVersion === 'range' && (
               <div className={classnames(classes.thumbMaxLabel, { [classes.show]: showMax })}>{maxVal}</div>
             )}
           </>
-        )}
-        {thumbLabels === 'inputs' && (
+        )}*/}
+        {/*{thumbLabels === 'inputs' && (
           <>
             <div data-testid="testThumbInputID"
               className={classnames(classes.inputMinValue, { [classes.show]: showMin, [classes.error]: error })}>
@@ -243,7 +406,7 @@ export const Slider = (props: ISlider) => {
               </div>
             )}
           </>
-        )}
+        )}*/}
       </div>
     </div>
   );
