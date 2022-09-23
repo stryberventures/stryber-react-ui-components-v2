1b895dc13a16c7c3b6b1ef42ad5ce462b8772d23
YaroslavHos <yaroslav.hos@stryber.com>
2022-09-23 14:03:54 +0200

    Added input highlight on toggle icon click

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -36,7 +36,7 @@ const Dropdown = forwardRef((props: IDropdown, ref: ForwardedRef<IDropdownRef>) 
     inputReadOnly = true, children, label, placeholder, value, className, color, name, fullWidth,
     hint, error, disabled, onClick, onToggle, contentClassName, onInputChange, ...rest
   } = props;
-  const classes = useStyles();
+  const classes = useStyles(props);
   const { open, onInputClick, onOverlayClick } = useDropdown(props, ref);
 
   return (
@@ -58,7 +58,7 @@ const Dropdown = forwardRef((props: IDropdown, ref: ForwardedRef<IDropdownRef>) 
         hint={hint}
         errorMessage={error}
         onChange={onInputChange}
-        className={classNames(classes.input, { [classes.inputDisabled]: disabled })}
+        className={classNames(classes.input, { [classes.inputDisabled]: disabled, [classes.focus]: open })}
         endAdornment={(
           <div className={classNames(classes.toggleIcon, {
             [classes.toggleIconDisabled]: disabled,
@@ -80,5 +80,7 @@ const Dropdown = forwardRef((props: IDropdown, ref: ForwardedRef<IDropdownRef>) 
 });
 
 Dropdown.displayName = 'Dropdown';
-
+Dropdown.defaultProps = {
+  color: 'primary',
+}
 export default Dropdown;
