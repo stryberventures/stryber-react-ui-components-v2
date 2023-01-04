16c197710769aabd7632e70c7c3e596053788223
AlinaKuzmenko <aline.kuzmenko@gmail.com>
2023-01-04 17:09:32 +0100

    remove unused types

--- src/components/Icons/types.ts
+++ src/components/Icons/types.ts
@@ -1,7 +1,3 @@
-import { SVGProps } from 'react';
-
-export type TPathDProp = 'd';
-
 export type TSingleVariants = 'default';
 
 export type TMoreVariants = 'vertical' | 'horizontal';
@@ -11,7 +7,3 @@ export type TCursorVariants = 'arrow' | 'drag' | 'hover';
 export type IArrowIconVariant = 'down' | 'up' | 'left' | 'right';
 
 export type TCommonIconVariants = 'default' | 'filled';
-
-export interface ICommonIconProps<Variants> extends SVGProps<SVGSVGElement> {
-  variant?: Variants;
-}
