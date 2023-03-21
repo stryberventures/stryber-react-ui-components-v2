9fad2a591763d89047a15ca27a0b2d672f2771f6
AlinaKuzmenko <aline.kuzmenko@gmail.com>
2023-03-21 18:06:27 +0100

    move breakpoints out of Grid

--- src/components/Theme/defaultTheme.ts
+++ src/components/Theme/defaultTheme.ts
@@ -120,14 +120,14 @@ export const defaultTheme: ITheme = {
     margin: defaultMargins,
     gap: defaultGaps,
     maxWidth: 1320,
-    breakpoints: {
-      xs: 0,
-      sm: 576,
-      md: 768,
-      lg: 992,
-      xl: 1200,
-      xxl: 1400,
-    },
+  },
+  breakpoints: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+    xxl: 1400,
   },
   spacing: {
     2: toRem(2),
